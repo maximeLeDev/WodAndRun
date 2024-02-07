3e1b9b73f50db6fb3dd71bf4bbd596b6a44dd2b1
POUILLARDmaxime <maxime.pouillard18@gmail.com>
2024-02-07 20:07:50 +0100

    Page d'acceuil avec component MenuEntryView

--- app/src/main/java/com/example/wodrun/MainActivity.kt
+++ app/src/main/java/com/example/wodrun/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.wodrun
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val menuEntryRun = findViewById<MenuEntryView>(R.id.menuEntryRun)
+        menuEntryRun.destination = WodTrainingActivity::class.java;
+        menuEntryRun.changeImage(R.drawable.pisteathletisme)
+        menuEntryRun.changeText(R.string.run)
+
+        val menuEntryCrossfit = findViewById<MenuEntryView>(R.id.menuEntryCrossfit)
+        menuEntryCrossfit.destination = WodTrainingActivity::class.java;
+        menuEntryCrossfit.changeImage(R.drawable.sallecrossfit)
+        menuEntryCrossfit.changeText(R.string.wod)
+
     }
 }
---
+++ app/src/main/java/com/example/wodrun/MenuEntryView.kt
@@ -0,0 +1,43 @@
+package com.example.wodrun
+
+import android.content.Context
+import android.content.Intent
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import kotlin.reflect.KClass
+
+class MenuEntryView(context : Context, attrs: AttributeSet) : FrameLayout(context, attrs) {
+
+    lateinit var destination : Class<*>
+
+    init {
+      inflate(context, R.layout.view_menu_entry, this)
+
+        this.setOnClickListener{
+            val intent = Intent(context, destination)
+            context.startActivity(intent)
+        }
+        this.setOnTouchListener { v: View, m: MotionEvent ->
+            // Perform tasks here
+            if (m.action == MotionEvent.ACTION_DOWN) {
+                v.alpha = 0.5f
+                true
+            } else if (m.action == MotionEvent.ACTION_UP) {
+                v.alpha = 1.0f
+                true
+            }
+            false
+        }
+    }
+    fun changeImage(resId: Int) {
+        findViewById<ImageView>(R.id.imageMenuEntry).setImageResource(resId)
+    }
+    fun changeText(resId: Int) {
+        findViewById<TextView>(R.id.textMenuEntry).setText(resId)
+    }
+}
